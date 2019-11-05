9f2ec1bf8a1bb4d658806dcfbadf6633aa6ea86a
Augustine Smith <absaugustineflash@gmail.com>
2019-11-05 21:08:39 +0000

    KV lang

--- app.py
+++ app.py
@@ -10,19 +10,14 @@ green = [0, 1, 0, 1]
 blue = [0, 0, 1, 1]
 purple = [1, 0, 1, 1]
 
-class MainApp(App):
+class ButtonApp(App):
 
     def build(self):
-        button = Button(text="Hello from Kivy",
-                    size_hint=(.5, .5),
-                    pos_hint={"center_x": .5, "center_y": .5}
-        )
-        button.bind(on_press=self.on_press_button)
-        return button
+        return Button()
 
     def on_press_button(self, instance):
         print("You pressed the button")
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = ButtonApp()
     app.run()
