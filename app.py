ef8abb17f19c81ea2176da0ac2e2888695b4a539
Augustine Smith <absaugustineflash@gmail.com>
2019-11-05 20:51:52 +0000

    Images in Kivy

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from kivy.app import App
 from kivy.uix.label import Label
-
+from kivy.uix.image import Image
 
 class MainApp(App):
     def build(self):
-        label = Label(text="Hello from Kivy",
-                        size_hint=(.5, .5),
-                        pos_hint={"center_x": .5, "center_y": .5})
-        return label
+        img = Image(source="./Dayllon.jpg",
+                size_hint=(1, 1),
+                pos_hint={"center_x": .5, "center_y": .5})
+        return img
 
 if __name__ == "__main__":
     app = MainApp()
