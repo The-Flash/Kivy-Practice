d332f07c3e308fca339bfbd9bf757d37bacc851c
Augustine Smith <absaugustineflash@gmail.com>
2019-11-05 21:02:37 +0000

    Button press event

--- app.py
+++ app.py
@@ -10,21 +10,19 @@ green = [0, 1, 0, 1]
 blue = [0, 0, 1, 1]
 purple = [1, 0, 1, 1]
 
-class HBoxLayoutExample(App):
-    def build(self):
-        layout = BoxLayout(
-            padding=10,
-            orientation="vertical"
-        )
-        colors = [red, green, blue, purple]
+class MainApp(App):
 
-        for i in range(5):
-            btn = Button(text="Button #%s"%(i+1),
-                        background_color = random.choice(colors)
-                    )
-            layout.add_widget(btn)
-        return layout
+    def build(self):
+        button = Button(text="Hello from Kivy",
+                    size_hint=(.5, .5),
+                    pos_hint={"center_x": .5, "center_y": .5}
+        )
+        button.bind(on_press=self.on_press_button)
+        return button
+
+    def on_press_button(self, instance):
+        print("You pressed the button")
 
 if __name__ == "__main__":
-    app = HBoxLayoutExample()
+    app = MainApp()
     app.run()
