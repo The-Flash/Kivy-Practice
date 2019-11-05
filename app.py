0f6b880b6f4c22f9593b94d11aa18ffdf24b116d
Augustine Smith <absaugustineflash@gmail.com>
2019-11-05 20:57:59 +0000

    Horizontal Box Layout

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.image import Image
+import kivy
+import random
 
-class MainApp(App):
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+red = [1, 0, 0, 1]
+green = [0, 1, 0, 1]
+blue = [0, 0, 1, 1]
+purple = [1, 0, 1, 1]
+
+class HBoxLayoutExample(App):
     def build(self):
-        img = Image(source="./Dayllon.jpg",
-                size_hint=(1, 1),
-                pos_hint={"center_x": .5, "center_y": .5})
-        return img
+        layout = BoxLayout(
+            padding=10,
+            orientation="vertical"
+        )
+        colors = [red, green, blue, purple]
+
+        for i in range(5):
+            btn = Button(text="Button #%s"%(i+1),
+                        background_color = random.choice(colors)
+                    )
+            layout.add_widget(btn)
+        return layout
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = HBoxLayoutExample()
     app.run()
